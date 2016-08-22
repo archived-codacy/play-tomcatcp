c9ae62bb64e75fb188208e57f32d700c2ba0f3b1
Daniel Reigada <daniel@codacy.com>
2016-08-22 13:17:46 +0100

    Add a wrapper class for DataSource

---
+++ module-code/app/com/codacy/play/tomcatcp/pool/TomcatCPDataSource.java
@@ -0,0 +1,97 @@
+package com.codacy.play.tomcatcp.pool;
+
+import com.codacy.play.tomcatcp.metrics.MetricsTrackerFactory;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.health.HealthCheckRegistry;
+import org.apache.tomcat.jdbc.pool.ConnectionPool;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolConfiguration;
+
+import java.sql.SQLException;
+
+public class TomcatCPDataSource extends DataSource {
+
+    private MetricsTrackerFactory metricsTrackerFactory;
+    private MetricRegistry metricRegistry;
+    private HealthCheckRegistry healthCheckRegistry;
+    private TomcatCPPool TCPool;
+
+
+    public TomcatCPDataSource() {
+        super();
+    }
+
+
+    public TomcatCPDataSource(PoolConfiguration poolProperties) {
+        super(poolProperties);
+    }
+
+    @Override
+    public ConnectionPool createPool() throws SQLException {
+        if (pool != null) {
+            return pool;
+        } else {
+            TCPool = new TomcatCPPool(poolProperties);
+            pool = TCPool;
+            return pool;
+        }
+    }
+
+
+    public MetricRegistry getMetricRegistry() {
+        return metricRegistry;
+    }
+
+    public void setMetricRegistry(MetricRegistry metricRegistry) {
+        if (metricsTrackerFactory != null) {
+            throw new IllegalStateException("cannot use setMetricRegistry() and setMetricsTrackerFactory() together");
+        }
+
+        boolean isAlreadySet = getMetricRegistry() != null;
+
+        if (pool != null) {
+            if (isAlreadySet) {
+                throw new IllegalStateException("MetricRegistry can only be set one time");
+            } else {
+                TCPool.setMetricRegistry(metricRegistry);
+            }
+        }
+    }
+
+    public MetricsTrackerFactory getMetricsTrackerFactory() {
+        return metricsTrackerFactory;
+    }
+
+    public void setMetricsTrackerFactory(MetricsTrackerFactory metricsTrackerFactory) {
+        if (metricRegistry != null) {
+            throw new IllegalStateException("cannot use setMetricsTrackerFactory() and setMetricRegistry() together");
+        }
+
+        boolean isAlreadySet = getMetricsTrackerFactory() != null;
+
+        if (pool != null) {
+            if (isAlreadySet) {
+                throw new IllegalStateException("MetricsTrackerFactory can only be set one time");
+            } else {
+                TCPool.setMetricsTrackerFactory(metricsTrackerFactory);
+            }
+        }
+    }
+
+    public HealthCheckRegistry getHealthCheckRegistry() {
+        return healthCheckRegistry;
+    }
+
+    public void setHealthCheckRegistry(HealthCheckRegistry healthCheckRegistry) {
+        boolean isAlreadySet = getHealthCheckRegistry() != null;
+
+        if (pool != null) {
+            if (isAlreadySet) {
+                throw new IllegalStateException("HealthCheckRegistry can only be set one time");
+            } else {
+                TCPool.setHealthCheckRegistry(healthCheckRegistry);
+            }
+        }
+    }
+
+}
